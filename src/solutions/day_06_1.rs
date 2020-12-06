e1819636a2dabb197c6b321f0df3973206be8837
Shawn White <shawn@capnrat.com>
2020-12-06 13:12:39 +0100

    Add some comments about how day 6 iter chaining works

--- src/solutions/day_06_1.rs
+++ src/solutions/day_06_1.rs
@@ -2,9 +2,24 @@ use crate::solutions::input::get_data_string;
 use itertools::Itertools;
 
 pub fn run() -> String {
+    // grabs the entire input as a single String
     get_data_string("input/input_06")
+        // split each group of people by the empty line in the input. This will return an iterator which allows me to operate on each group
         .split("\n\n")
-        .map(|s| s.chars().filter(|c| !c.is_whitespace()).unique().count())
+        // taking the group iterator from above, use map to convert each group into how many unique "yes" answers are in each group
+        .map(
+            // map will pass each group as a string |s| into an operation.
+            // s.chars() returns in an iter of all the characters in the string. This is all of the "yes" answers given by the group
+            |s| s.chars()
+                // we have some new lines still in the chars that we don't care about. this filters them out.
+                .filter(|c| !c.is_whitespace())
+                // itertools provides this .unique function which will provide an iterator for all of the unique elements in an iteration
+                // so this .unique().count() will count how many unique "yes" answers there are
+                // this is returned as the value that the map will convert the group to
+                .unique().count()
+        )
+        // now that we have a bunch of unique answer counts for each group. we sum them all together for the answer
         .sum::<usize>()
+        // convert to string because my framework expects a string as a result
         .to_string()
 }
--- src/solutions/day_06_2.rs
+++ src/solutions/day_06_2.rs
@@ -2,13 +2,25 @@ use crate::solutions::input::get_data_string;
 use itertools::Itertools;
 
 pub fn run() -> String {
+    // grabs the entire input as a single String (my input handling strips all the \r before this on windows which is important for the rest of this to work)
     get_data_string("input/input_06")
+        // split each group of people by the empty line in the input. This will return an iterator which allows me to operate on each group
         .split("\n\n")
-        .map(|group| group.chars().filter(|&c| c != '\n')
+        // taking the group iterator from above, use map to convert each group into how many unique "yes" answers are shared for all members of a group
+        .map(|group| group.chars()
+            // filter out all the new lines, we don't care about them
+            .filter(|&c| c != '\n')
+            // using itertools create an iterator that only has one element for each unique answer in the group
             .unique()
+            // convert each of those unique answers into how many times that answer was answered in that group
             .map(|answer| group.chars().filter(|&c| c == answer).count())
+            // we know how many people are in a group by the number of lines in the group string.
+            // compare the number of people to the count of each answer. filter out any that aren't equal, because that means at least one person did not answer yes to that question
             .filter(|&count| count == group.lines().count())
-            .count()
-        ).sum::<usize>()
+            // now we have an iterator of counts that all equal the number of people in the group
+            // count how many of them there are and that's how many "yes" answers were answered by the entire group
+            .count())
+        // add the counts from each group together for the answer
+        .sum::<usize>()
         .to_string()
 }
